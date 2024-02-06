30b9bf1b26a58d090249d33bcfd503110f709cc5
V1daa <mrjokerekoj@gmail.com>
2024-02-06 15:13:01 +0100

    Version Nav Fixed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           </span>
         </a>
         <div className="w-[500px] h-full flex flex-row items-center justify-betwen md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <div className=" absolute xl:left-[40%] lg:left-[37.5%] flex items-center  justify-between w-[400px] h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 md:left-[18%]">
             <a href="#hero" className="cursor-pointer">
               About me
             </a>
