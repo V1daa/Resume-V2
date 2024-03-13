751f297f706b825c9a1662bbeee4a8f2fe57abbd
V1daa <mrjokerekoj@gmail.com>
2024-03-13 16:40:50 +0100

    descriptions

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,25 +10,37 @@ const Projects = () => {
           My projects
         </h1>
         <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <Link href="https://board-eight-blush.vercel.app" target="_blank" className="cursor-pointer z-[80]">
-          <ProjectCard
-            src="/board.jpg"
-            title="Boardy"
-            description="Perfect app for big companies and groups of sudents. Plan you'r buissnes strategy, apointments, prepare for exams with you'r group in real time and more. Created with support of convex database and clerk authentification"
-          />
+          <Link
+            href="https://board-eight-blush.vercel.app"
+            target="_blank"
+            className="cursor-pointer z-[80]"
+          >
+            <ProjectCard
+              src="/board.jpg"
+              title="Boardy"
+              description="Perfect app for big companies and groups of sudents. Created with use of clerk authentification and convex real-time easy and fast database. Zustand was used to manage states. And liveblocks to power everything up. "
+            />
           </Link>
-          <Link href="https://notion-henna-mu.vercel.app" target="_blank" className="cursor-pointer z-[80]">
-          <ProjectCard
-            src="/notion.jpg"
-            title="Notion with multicursor"
-            description="Full-stack notion copy with multicursor. Perfect orginizer for group projects or for big groups of students. Product is ready for deploy and commercial usage. Saas web-page with backend authentication. Perfect for collaborations "
-          />
+          <Link
+            href="https://notion-henna-mu.vercel.app"
+            target="_blank"
+            className="cursor-pointer z-[80]"
+          >
+            <ProjectCard
+              src="/notion.jpg"
+              title="Notion with multicursor"
+              description="Full-stack notion copy with multicursor. Perfect orginizer for group projects or for big groups of students. Product is build with use of supabase, socket.io, drizzle ORD and tailwind. Stripe would enable payments in next versions "
+            />
           </Link>
-          <Link href="https://all-in-one-five.vercel.app" target="_blank" className="cursor-pointer z-[80]">
+          <Link
+            href="https://all-in-one-five.vercel.app"
+            target="_blank"
+            className="cursor-pointer z-[80]"
+          >
             <ProjectCard
               src="/AllInOne.jpg"
               title="All In One App"
-              description="In this app you can find everything you need on daily basis and even more. App is in non-stop progression to fill all human needs in one place. Currently web-page is in early beta stage I'll deploy more tools for every human needs!"
+              description="In this app you can find everything you need on daily basis and even more. App is in non-stop progression to fill all human needs in one place. Build with react and just raw algoritms, open weather map was used to create the web app. "
             />
           </Link>
         </div>
